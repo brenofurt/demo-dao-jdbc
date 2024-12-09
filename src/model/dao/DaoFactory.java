3070d8ea35ce81b80ac00b30dd6a867dd8e43a8e
brenofurt <brenofurtrodrigues@gmail.com>
2024-12-09 01:16:46 -0300

    findById implemantation

--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,10 +1,11 @@
 package model.dao;
 
+import db.DB;
 import model.dao.impl.SellerDaoJDBC;
 
 public class DaoFactory {
 
 	public static SellerDao createSellerDao() { //aqui o retorno é do tipo da interface que na verdade é um sellerdaoJDBC
-		return new SellerDaoJDBC();
+		return new SellerDaoJDBC(DB.getConnection());
 	}
 }
